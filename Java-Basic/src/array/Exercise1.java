1d67d170cb4149ec59f7ebe93b4ba368428648b6
JangHwanPark <wkd5878@gmail.com>
2024-03-18 20:56:17 +0900

    JavaBasic - Java Array

--- Java-Basic/src/array/Exercise1.java
+++ Java-Basic/src/array/Exercise1.java
@@ -1,15 +1,21 @@
 package array;
 
 class Exercise01 {
-    public void printExercise01() {
-        String[] cars = {"Volvo", "BMW", "KIA", "Ford", "Hyundai"};
-        System.out.println("cars: " + cars[1]);
+    public void printExercise01(String[] cars) {
+        System.out.println("cars: " + cars[0]);
+    }
+
+    public void printChangeValue(String[] cars) {
+        cars[0] = "Ope1";
+        System.out.println("ChangeValue(cars): " + cars[0]);
     }
 }
 
 public class Exercise1 {
     public static void main(String[] args) {
+        String[] cars = {"Volvo", "BMW", "KIA", "Ford", "Hyundai"};
         Exercise01 exam01 = new Exercise01();
-        exam01.printExercise01();
+        exam01.printExercise01(cars);
+        exam01.printChangeValue(cars);
     }
 }
