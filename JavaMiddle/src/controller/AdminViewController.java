eb29210469cc919695a2dffd109433bef805a5d4
JangHwanPark <wkd5878@gmail.com>
2024-05-12 10:43:52 +0900

    refactoring: 중복 코드 제거, 매직넘버 제거, foreach 루프로 변경

--- JavaMiddle/src/controller/AdminViewController.java
+++ JavaMiddle/src/controller/AdminViewController.java
@@ -9,30 +9,31 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 public class AdminViewController {
+    private static final String[] columnNames = {"고객 ID", "이름", "이메일", "전화번호", "주소", "주문 ID", "주문 날짜", "주문 상태"};
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
     public static String[] getColumnNames() {
-        return new String[]{"고객 ID", "이름", "이메일", "전화번호", "주소", "주문 ID", "주문 날짜", "주문 상태"};
+        return columnNames;
     }
 
     public static Object[][] prepareTableData(ArrayList<OrdersDTO> orders) {
         // 테이블 열 이름, 데이터 배열 생성
-        String[] columnNames = getColumnNames();
         Object[][] data = new Object[orders.size()][columnNames.length];
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
         // 고객 데이터를 테이블 데이터 배열에 할당
-        for (int i = 0; i < orders.size(); i++) {
-            OrdersDTO order = orders.get(i);
+        for (OrdersDTO order : orders) {
             CustomerDTO customer = order.getCustomer();
             ProductDTO product = order.getProduct();
+            int index = orders.indexOf(order);
 
-            data[i][0] = customer.getUserId();
-            data[i][1] = customer.getName();
-            data[i][2] = customer.getEmail();
-            data[i][3] = customer.getPhone();
-            data[i][4] = customer.getAddress();
-            data[i][5] = product.getModel();  // 제품 모델 추가
-            data[i][6] = dateFormat.format(order.getPurchaseDate());  // 주문 날짜
-            data[i][7] = order.isPaymentStatus() ? "결제 완료" : "결제 대기";  // 주문 상태
+            data[index][0] = customer.getUserId();
+            data[index][1] = customer.getName();
+            data[index][2] = customer.getEmail();
+            data[index][3] = customer.getPhone();
+            data[index][4] = customer.getAddress();
+            data[index][5] = product.getModel();
+            data[index][6] = dateFormat.format(order.getPurchaseDate());
+            data[index][7] = order.isPaymentStatus() ? "결제 완료" : "결제 대기";
         }
 
         return data;
